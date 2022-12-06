7bd41fc5aea17b8a06d95245d20aff2bd8014a53
Alyeldeen68 <alyeldeenkhaled2@gmail.com>
2022-12-06 23:54:02 +0200

    Intialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Components/Landing Page/NavBar.js";
+import Body from "./Components/Landing Page/Body";
+import CreateProject from "./Components/Create Project/CreateProject";
+import Volanter from "./Components/Volanteer Page/Volanter";
+import { Routes, Route } from "react-router-dom";
+import VolanterCustom from "./Components/Volanteer Page/VolanterCustom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Volanter />} />
+        <Route path="/about" element={<div>hello</div>} />
+        <Route path="/data" element={<VolanterCustom />} />
+      </Routes>
+    </>
   );
 }
 
